b3226e8788203bd0d758d38b843310b069ec9774
sachith.vidu@yahoo.com <sachith.vidu@yahoo.com>
2024-03-31 16:06:22 +0530

    Code refactor

--- src/main/java/io/github/sachithariyathilaka/S3TutorialApplication.java
+++ src/main/java/io/github/sachithariyathilaka/S3TutorialApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @class       S3TutorialApplication
- * @description Main class of the application
+ * Main class of the application
  *
- * @author      Sachith Ariyathilaka
- * @version     1.0.0
- * @date        2024/03/17
+ * @author	Sachith Ariyathilaka
+ * @version	1.0.0
+ * @since	2024/03/17
  */
 @SpringBootApplication
 public class S3TutorialApplication {
